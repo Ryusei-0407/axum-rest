b69b927227ec2b59a731649c9a0998bb7be435d8
ryu <ryu000407@gmail.com>
2022-02-07 12:26:06 +0900

    change id to uuid

--- migrations/20220205185921_user.up.sql
+++ migrations/20220205185921_user.up.sql
@@ -1,8 +1,9 @@
 -- Add up migration script here
 CREATE TABLE user_table (
-  id serial NOT NULL PRIMARY KEY,
+  uuid uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   name VARCHAR(50) NOT NULL,
   age INT NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 );
+

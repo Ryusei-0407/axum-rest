1f098d24fdef1ea8bab5aa49f59d2277d7734cd8
ryu <ryu000407@gmail.com>
2022-02-07 18:27:14 +0900

    change database postgres to cockroachdb

--- migrations/20220205185921_user.up.sql
+++ migrations/20220205185921_user.up.sql
@@ -1,6 +1,6 @@
 -- Add up migration script here
 CREATE TABLE user_table (
-  uuid uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  uuid uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   name VARCHAR(50) NOT NULL,
   age INT NOT NULL,
   created_at TIMESTAMP NOT NULL,
